51d443c3574b718659070382b4ce116d981435de
haishisai <haishisai@qq.com>
2020-01-14 23:03:17 +0800

    icons-save

---
+++ src/components/icons.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="icons-box">
+        <div class="icon">
+            <div class="icon-img">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
+            </div>
+            <div class="icon-txt">xxx</div>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
+            </div>
+            <div class="icon-txt">xxx</div>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
+            </div>
+            <div class="icon-txt">xxx</div>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
+            </div>
+            <div class="icon-txt">xxx</div>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
+            </div>
+            <div class="icon-txt">xxx</div>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
+            </div>
+            <div class="icon-txt">xxx</div>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
+            </div>
+            <div class="icon-txt">xxx</div>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
+            </div>
+            <div class="icon-txt">xxx</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'homeIcons'
+}
+</script>
+<style lang="stylus" scoped>
+.icons-box
+    height:0
+    padding-bottom: 50%
+    // 100% 对应通栏宽度
+    overflow:hidden
+    background-color: #232323
+    .icon
+        height 0
+        float:left
+        width:25%
+        padding-bottom:25%
+        background :red
+        // overflow:hidden
+        .icon-img
+            background:blue
+            width:75%
+            margin: 0 auto
+            img
+                width:100%
+        .icon-txt
+            background:green
+            text-align:center
+            font-size :16px
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <homeHeader></homeHeader>
     <home-swiper></home-swiper>
-
+    <home-icons></home-icons>
     <div id="test">
       <router-link to="./list">跳转 从home 路由到 list.vue → list_part1.vue 只是一个部件</router-link>
       <br />
@@ -17,13 +17,15 @@
 // @ is an alias to /src
 import homeHeader from "../components/header.vue";
 import homeSwiper from "../components/swiper.vue";
+import homeIcons from "../components/icons.vue";
 import axios from "axios";
 export default {
   name: "home",
   components: {
     homeHeader: homeHeader,
     //可以直接写个变量名注册  效果  aaa1 ：aaa 一样
-    homeSwiper
+    homeSwiper,
+    homeIcons
   },
   methods: {
     getHomeInfo() {
